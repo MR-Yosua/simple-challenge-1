5140d24f8cd1bbfba5d39387d0c10de5b13a54e5
Josue Lavado <j.lavadoortubia@gmail.com>
2020-08-22 14:53:14 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Nota para el desafío
+
+## Lo que se pretende en este desafío es cumplir lo siguiente:
+un grupo decide organizar un juego de estrategia,lo importante es que estos integrantes secretos
+se comunicaran mediante un canal común, por lo que deben cifrar sus mensajes para otros equipos
+Uno de los miembros plantea un metodo de encriptación llamado "la cifra del cesar" que consiste
+en correr cada letra del mensaje en x posiciones, ejemplo si la palabra es "ATAQUE" se transforma en "CVCSWG"
+El jefe decide cuantas cantidades de cifras se correran las letras
+
+## NOTA: si el alfavbeto termina antes de correr la cantidad necesaria, se comienza nuevamente por la letra a
+## Ejemplo : "EXTRA" -> 3 se convierte en "HAWUD" notar que X+3 letras -> A
+
+
---
+++ main.py
@@ -0,0 +1,42 @@
+
+def solve(cadena,step):
+    CADENA_FINAL= []
+    #recorrer la cadena caracter por caracter
+    for letra in cadena:
+        #se recorre un contador desde el 65 a 91 que corresponde a los codigos ascci de las mayusculas
+        for index in range (65 , 91):
+            #se realiza la comparación de la letra con el rango para ver si pertenece
+            if letra == chr(index):
+                # se valida que la letra con los pasos al aumentar es mayor al rango
+                if index + step > 90:
+                    #se resetea a la primera letra conservando los pasos
+                    reset = ((index + step) - 91) + 65
+                    CADENA_FINAL.append(chr(reset))
+                    #print(chr(reset))
+                else:
+                    #si no es mayor que el rango lo imprime
+                    CADENA_FINAL.append(chr(index+step))
+                    #print(chr(index+step))
+
+        #Mismas condiciones anteriores !
+        for index2 in range (97,123) :
+            if letra == chr(index2):
+                if index2 + step > 122:
+                    reset = ((index2 + step) - 123) + 97
+                    CADENA_FINAL.append(chr(reset))
+                    #print(chr(reset))
+                else:
+                    CADENA_FINAL.append(chr(index2+step))
+                    #print(chr(index2+step))
+    separator = ''
+    print("Tu mensaje secreto es :")
+    print(separator.join(CADENA_FINAL))
+
+def main():
+    print("ingrese mensaje : ")
+    cadena = input()
+    print("Ingrese parametro clave (INT) : ")
+    step = input()
+    solve(cadena,int(step))
+
+main()
