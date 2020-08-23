8caa37eb2348a4b7d5896f9679c54017695017f9
Josue Lavado <j.lavadoortubia@gmail.com>
2020-08-23 13:32:17 -0400

    agrega los caracteres no letras

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
 def solve(cadena,step):
     CADENA_FINAL= []
+    
     #recorrer la cadena caracter por caracter
     for letra in cadena:
+        
+        if(letra.isalpha() == False):
+            CADENA_FINAL.append(letra)
+
         #se recorre un contador desde el 65 a 91 que corresponde a los codigos ascci de las mayusculas
         for index in range (65 , 91):
             #se realiza la comparación de la letra con el rango para ver si pertenece
@@ -17,7 +22,7 @@ def solve(cadena,step):
                     #si no es mayor que el rango lo imprime
                     CADENA_FINAL.append(chr(index+step))
                     #print(chr(index+step))
-
+                    
         #Mismas condiciones anteriores !
         for index2 in range (97,123) :
             if letra == chr(index2):
@@ -28,6 +33,8 @@ def solve(cadena,step):
                 else:
                     CADENA_FINAL.append(chr(index2+step))
                     #print(chr(index2+step))
+            # else:
+            #     CADENA_FINAL.append(letra)
     separator = ''
     print("Tu mensaje secreto es :")
     print(separator.join(CADENA_FINAL))
